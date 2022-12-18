344ae99a584d77154be8ca1a0f3cbf2761af8306
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-18 22:35:09 +0200

    Add ReactionButtons component

---
+++ src/components/ReactionButtons.js
@@ -0,0 +1,30 @@
+import { useDispatch } from "react-redux";
+import { reactionAdded } from "../feautures/posts/postsSlice";
+
+const reactionEmoji = {
+    thumbsUp: 'ok',
+    wow: 'wow',
+    heart: 'heart',
+    rocket: 'rocket',
+    coffee: 'coffee'
+}
+
+export const ReactionButtons = ({ post }) => {
+    const dispatch = useDispatch();
+    const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+        return (
+            <button
+                key={name}
+                type='button'
+                onClick={() =>
+                    dispatch(reactionAdded({ postId: post.id, reaction: name }))}
+            >
+                {emoji} {post.reactions[name]}
+            </button>
+        )
+    })
+
+    return (
+        <div>{reactionButtons}</div>
+    );
+}
