ca394d47cb6c9c329723ff8280e90167fac9e957
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-18 21:54:54 +0200

    Add users reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../feautures/counter/counterSlice';
 import postsReducer from '../feautures/posts/postsSlice';
+import usersReducer from '../feautures/users/usersSlice';
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
-        posts: postsReducer
+        posts: postsReducer,
+        users: usersReducer
     }
 });
