1af8b2a137eccd00cc61c74a971d0c5a6ef00424
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-18 22:35:36 +0200

    Render reactions, post author and post time ago

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,16 +1,24 @@
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "../feautures/posts/postsSlice";
 import { PostAuthor } from "./PostAuthor";
+import { ReactionButtons } from "./ReactionButtons";
+import { TimeAgo } from "./TimeAgo";
 
 export const PostsList = () => {
     const posts = useSelector(selectAllPosts);
+
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
     return (
         <section>
             <h2>Posts</h2>
-            {posts.map(post => (<article key={post.id}>
+            {orderedPosts.map(post => (<article key={post.id}>
                 <h3>{post.title}</h3>
                 <p>{post.content}</p>
-                <p><PostAuthor userId={post.userId}/></p>
+                <p>
+                    <PostAuthor userId={post.userId} />
+                    <TimeAgo timestamp={post.date} />
+                </p>
+                <ReactionButtons post={post}/>
             </article>))}
         </section>
     );
