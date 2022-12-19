785b3b31e4b232ebeba27b58ff1f827e2a29ac15
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-19 11:27:14 +0200

    Wrap app with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { fetchUsers } from './feautures/users/usersSlice';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 store.dispatch(fetchUsers());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/*' element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </Provider>
 );
