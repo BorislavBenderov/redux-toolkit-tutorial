1a4ac3f3d31be53099864fe4a69918c3837a1db1
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-19 10:33:51 +0200

    Create async thunk to add data

--- src/feautures/posts/postsSlice.js
+++ src/feautures/posts/postsSlice.js
@@ -11,8 +11,22 @@ const initialState = {
 };
 
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
-    const response = await axios.get(POSTS_URL);
-    return response.data;
+    try {
+        const response = await axios.get(POSTS_URL);
+        return response.data;
+    } catch (error) {
+        return error.message;
+    }
+})
+
+export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPost) => {
+    try {
+        const response = await axios.post(POSTS_URL, initialPost);
+        console.log(initialPost);
+        return response.data;
+    } catch (error) {
+        return error.message;
+    }
 })
 
 const postsSlice = createSlice({
@@ -77,6 +91,20 @@ const postsSlice = createSlice({
             .addCase(fetchPosts.rejected, (state, action) => {
                 state.status = 'failed';
                 state.error = action.error.message;
+            })
+
+            .addCase(addNewPost.fulfilled, (state, action) => {
+                action.payload.userId = Number(action.payload.userId);
+                action.payload.date = new Date().toISOString();
+                action.payload.reactions = {
+                    thumbsUp: 0,
+                    wow: 0,
+                    heart: 0,
+                    rocket: 0,
+                    coffee: 0
+                }
+                console.log(action.payload);
+                state.posts.push(action.payload);
             })
     }
 });
