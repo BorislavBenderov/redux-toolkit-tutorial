2d3e53037ea4ba226ec1ddba929f5546e4a6296d
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-19 14:18:55 +0200

    Dispatch fetch posts

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { fetchUsers } from './feautures/users/usersSlice';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { fetchPosts } from './feautures/posts/postsSlice';
 
 store.dispatch(fetchUsers());
+store.dispatch(fetchPosts());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
