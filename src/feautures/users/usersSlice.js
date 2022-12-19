64df4918f13400c03b0301e4df4daa7af1ec7663
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-19 10:12:27 +0200

    Create async thunk to fetch users

--- src/feautures/users/usersSlice.js
+++ src/feautures/users/usersSlice.js
@@ -1,15 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-    { id: '0', name: 'John' },
-    { id: '2', name: 'Colin' }
-];
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+
+const initialState = [];
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    const response = await axios.get(USERS_URL);
+    return response.data;
+})
 
 const usersSlice = createSlice({
     name: 'users',
     initialState,
     reducers: {
 
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            return action.payload;
+        })
     }
 });
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { fetchUsers } from './feautures/users/usersSlice';
+
+store.dispatch(fetchUsers());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
