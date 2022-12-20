c513c9752a46a27af05f896779ccdbeeea127664
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-20 16:21:14 +0200

    Add TodoList component

---
+++ src/feautures/todos/TodoList.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { useGetTodosQuery } from '../api/apiSlice';
+
+const TodoList = () => {
+    const [newTodo, setNewTodo] = useState('')
+
+    const {
+        data: todos,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetTodosQuery();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //addTodo
+        setNewTodo('')
+    }
+
+    const newItemSection =
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="new-todo">Enter a new todo item</label>
+            <div className="new-todo">
+                <input
+                    type="text"
+                    id="new-todo"
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                    placeholder="Enter new todo"
+                />
+            </div>
+            <button className="submit">
+
+            </button>
+        </form>
+
+
+    let content;
+    if (isLoading) {
+        content = <p>Loading...</p>
+    } else if (isSuccess) {
+        content = JSON.stringify(todos);
+    } else if (isError) {
+        content = <p>{error}</p>
+    }
+
+    return (
+        <main>
+            <h1>Todo List</h1>
+            {newItemSection}
+            {content}
+        </main>
+    )
+}
+
+export default TodoList;
