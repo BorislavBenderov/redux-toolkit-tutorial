6083cc8bb61e1bb17b309010a681b3d8908fb903
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-20 16:21:25 +0200

    Render TodoList component

--- src/App.js
+++ src/App.js
@@ -6,26 +6,11 @@ import { SinglePostPage } from './feautures/posts/SinglePostPage';
 import { EditPostForm } from './feautures/posts/EditPostForm';
 import { UserList } from './feautures/users/UsersList';
 import { UserPage } from './feautures/users/UserPage';
+import TodoList from './feautures/todos/TodoList';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<PostsList />} />
-        <Route path='post'>
-          <Route index element={<AddPostForm />} />
-          <Route path=':postId' element={<SinglePostPage />} />
-          <Route path='edit/:postId' element={<EditPostForm />} />
-        </Route>
-
-        <Route path='user'>
-          <Route index element={<UserList />} />
-          <Route path=':userId' element={<UserPage />} />
-        </Route>
-
-        <Route path='*' element={<Navigate to='/' replace />}/>
-      </Route>
-    </Routes>
+    <TodoList />
   );
 }
 
