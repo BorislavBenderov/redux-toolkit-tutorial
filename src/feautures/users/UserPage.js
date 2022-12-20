71d12576ca67609120675978fee3ab1eb6976c78
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-20 14:13:42 +0200

    Fix user page rerendering

--- src/feautures/users/UserPage.js
+++ src/feautures/users/UserPage.js
@@ -1,15 +1,12 @@
 import { useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import { selectUserById } from "./usersSlice";
-import { selectAllPosts } from '../posts/postsSlice';
+import { selectAllPosts, selectPostsByUser } from '../posts/postsSlice';
 
 export const UserPage = () => {
     const { userId } = useParams();
     const user = useSelector(state => selectUserById(state, Number(userId)));
-    const postsForUser = useSelector(state => {
-        const allPosts = selectAllPosts(state);
-        return allPosts.filter(post => post.userId === Number(userId));
-    })
+    const postsForUser = useSelector(state => selectPostsByUser(state, Number(userId)));
 
     const postTitles = postsForUser.map(post => (
         <li key={post.id}>
