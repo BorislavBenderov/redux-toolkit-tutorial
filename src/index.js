5d9e8b42a6c43bc1b60819c57e3d4603c4c6667a
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-20 16:20:54 +0200

    Wrap App with ApiProvider

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux';
 import { fetchUsers } from './feautures/users/usersSlice';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { fetchPosts } from './feautures/posts/postsSlice';
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { apiSlice } from './feautures/api/apiSlice';
 
 store.dispatch(fetchUsers());
 store.dispatch(fetchPosts());
@@ -13,10 +15,12 @@ store.dispatch(fetchPosts());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/*' element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <ApiProvider api={apiSlice}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </ApiProvider>
   </Provider>
 );
